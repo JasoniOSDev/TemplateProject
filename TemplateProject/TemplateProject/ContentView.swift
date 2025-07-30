9c4d71d31f2b7ad679ed616fb3342b328a5fdd96
Jasooon <jason.ios.dev@gmail.com>
2025-07-30 12:01:10 +0800

    Add files via upload

---
+++ TemplateProject/TemplateProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TemplateProject
+//
+//  Created by chenjiesheng on 2025/7/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TemplateProject/TemplateProject/TemplateProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplateProjectApp.swift
+//  TemplateProject
+//
+//  Created by chenjiesheng on 2025/7/30.
+//
+
+import SwiftUI
+
+@main
+struct TemplateProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
